438d2f120ef9f04753798cf4f25b986fae5e5275
Ace Lake <96988100+AceLake@users.noreply.github.com>
2024-04-21 21:26:37 -0700

    Updated the key

--- League-Of-Fools/Service/SummonerService.cs
+++ League-Of-Fools/Service/SummonerService.cs
@@ -11,7 +11,7 @@ namespace League_Of_Fools.Service
     {
         //coment
         //private static readonly HttpClient client;
-        private static string apiKey = "RGAPI-87c18454-42c8-4ac7-a6d9-3adfdf5ba82e";
+        private static string apiKey = "RGAPI-7a4c9f7c-1b6d-43b8-a602-de0b9178c450";
         private readonly IHttpClientFactory _clientFactory;
         private readonly IChampionService _championService;
 
